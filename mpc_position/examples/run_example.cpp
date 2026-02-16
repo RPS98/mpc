bcc61c415760737721f97335db97be1a970ed464
carmendrpr <c.derojas@upm.es>
2026-02-16 17:22:07 +0100

    new c++ example

--- mpc_position/examples/run_example.cpp
+++ mpc_position/examples/run_example.cpp
@@ -67,55 +67,38 @@ Eigen::Quaterniond compute_path_facing(const Eigen::Vector3d& current_position,
   return q_target;
 }
 
-void position_ref_to_mpc_ref(Eigen::Vector3d waypoint,
-                             acados_mpc::MPCData* mpc_data,
-                             int index,
-                             bool path_facing = false) {
+void set_mpc_reference_parameters(const Eigen::Vector3d& waypoint,
+                                  acados_mpc::MPC& mpc,
+                                  const acados_mpc::MPCData* mpc_data,
+                                  bool path_facing = false) {
+  // Get current orientation
   Eigen::Quaterniond current_orientation;
-  current_orientation.w()                = mpc_data->state.data[3];
-  current_orientation.x()                = mpc_data->state.data[4];
-  current_orientation.y()                = mpc_data->state.data[5];
-  current_orientation.z()                = mpc_data->state.data[6];
+  current_orientation.w() = mpc_data->state.data[3];
+  current_orientation.x() = mpc_data->state.data[4];
+  current_orientation.y() = mpc_data->state.data[5];
+  current_orientation.z() = mpc_data->state.data[6];
+
+  // Compute desired orientation
   Eigen::Quaterniond desired_orientation = current_orientation;
-  if (index == MPC_N) {
-    // Position
-    // mpc_data->reference_end.set_data(0, waypoint[0]);
-    // mpc_data->reference_end.set_data(1, waypoint[1]);
-    // mpc_data->reference_end.set_data(2, waypoint[2]);
-
-    // Orientation
-    if (path_facing) {
-      Eigen::Vector3d current_position;
-      current_position << mpc_data->state.data[0], mpc_data->state.data[1], mpc_data->state.data[2];
-      desired_orientation = compute_path_facing(current_position, waypoint, current_orientation);
-    }
-    mpc_data->p_params.set_data(MPC_N, 1, desired_orientation.w());
-    mpc_data->p_params.set_data(MPC_N, 2, desired_orientation.x());
-    mpc_data->p_params.set_data(MPC_N, 3, desired_orientation.y());
-    mpc_data->p_params.set_data(MPC_N, 4, desired_orientation.z());
-
-    return;
-  } else if (index > MPC_N) {
-    throw std::out_of_range("Index out of range.");
-  }
-  // // Position
-  // mpc_data->reference.set_data(index, 0, waypoint[0]);
-  // mpc_data->reference.set_data(index, 1, waypoint[1]);
-  // mpc_data->reference.set_data(index, 2, waypoint[2]);
-
-  // // Control
-  // mpc_data->reference.set_data(index, 9, mpc_data->p_params.data[0] * 9.81);  // Thrust
-
-  // Orientation
   if (path_facing) {
     Eigen::Vector3d current_position;
     current_position << mpc_data->state.data[0], mpc_data->state.data[1], mpc_data->state.data[2];
     desired_orientation = compute_path_facing(current_position, waypoint, current_orientation);
   }
-  mpc_data->p_params.set_data(index, 1, desired_orientation.w());
-  mpc_data->p_params.set_data(index, 2, desired_orientation.x());
-  mpc_data->p_params.set_data(index, 3, desired_orientation.y());
-  mpc_data->p_params.set_data(index, 4, desired_orientation.z());
+
+  // Set online parameters using the new interface methods
+  mpc.setSolverOnlineDesiredPositionParams({waypoint[0], waypoint[1], waypoint[2]});
+  mpc.setSolverOnlineDesiredOrientationParams({
+    desired_orientation.w(),
+    desired_orientation.x(),
+    desired_orientation.y(),
+    desired_orientation.z()
+  });
+  mpc.setSolverOnlineDesiredVelocityParams({0.0, 0.0, 0.0});  // Zero velocity reference
+  
+  // Set desired actuation (hovering thrust)
+  double mass = mpc.get_data()->p_params.data[0];
+  mpc.setSolverOnlineDesiredActuationParams({mass * 9.81, 0.0, 0.0, 0.0});
 }
 
 void print_progress_bar(float progress) {
@@ -132,6 +115,7 @@ void print_progress_bar(float progress) {
     }
   }
   std::cout << "] " << int(progress * 100.0) << " %\r";
+  std::cout.flush();
 }
 
 void test_mpc_controller(CsvLogger& logger,
@@ -160,50 +144,79 @@ void test_mpc_controller(CsvLogger& logger,
 
   int pos_index     = 0;
   double hover_time = 2.0;
+  
+  std::cout << "Starting MPC simulation..." << std::endl;
+  std::cout << "Total time: " << tg_max_time + hover_time << " s" << std::endl;
+  std::cout << "Time step: " << tf << " s" << std::endl;
+  std::cout << "Prediction steps: " << prediction_steps << std::endl;
+
   for (double t = 0; t < tg_max_time + hover_time; t += tf) {
     print_progress_bar(t / (tg_max_time + hover_time));
     auto iter_start = std::chrono::high_resolution_clock::now();
 
-    // yref from 0 to N-1 (N steps) and yref_N from N to N
-    for (int i = 0; i < prediction_steps + 1; i++) {
-      position_ref_to_mpc_ref(yaml_data.waypoints[pos_index], mpc_data, i, yaml_data.path_facing);
-    }
+    // Set reference parameters for all prediction stages
+    set_mpc_reference_parameters(
+      yaml_data.waypoints[pos_index], 
+      mpc, 
+      mpc_data, 
+      yaml_data.path_facing
+    );
+
     // Solve MPC
     auto mpc_start = std::chrono::high_resolution_clock::now();
-    mpc.solve();
+    int status = mpc.solve();
     auto mpc_end = std::chrono::high_resolution_clock::now();
+
+    if (status != 0) {
+      std::cerr << "\nMPC solver failed with status " << status << " at time " << t << std::endl;
+    }
 
     // Simulate
     auto sim_start = std::chrono::high_resolution_clock::now();
     simulator.solve(mpc_data);
     auto sim_end = std::chrono::high_resolution_clock::now();
 
+    // Time measurements
     std::chrono::duration<double> mpc_duration   = mpc_end - mpc_start;
     std::chrono::duration<double> sim_duration   = sim_end - sim_start;
     std::chrono::duration<double> total_duration = sim_end - iter_start;
     mpc_times.push_back(mpc_duration.count());
     sim_times.push_back(sim_duration.count());
     total_times.push_back(total_duration.count());
+    
+    // Log data
     logger.save(t, mpc_data);
+    
+    // Compute position error
     double error =
         std::sqrt(std::pow(mpc_data->state.data[0] - yaml_data.waypoints[pos_index][0], 2) +
                   std::pow(mpc_data->state.data[1] - yaml_data.waypoints[pos_index][1], 2) +
                   std::pow(mpc_data->state.data[2] - yaml_data.waypoints[pos_index][2], 2));
-    // If the error is less than 0.1 m, go to the next waypoint
+    
+    // Update waypoint if reached
     if (error < 0.1 && pos_index < yaml_data.waypoints.size() - 1) {
       pos_index++;
+      std::cout << "\nWaypoint " << pos_index << " reached at time " << t << " s" << std::endl;
+      std::cout << "Next target: [" 
+                << yaml_data.waypoints[pos_index][0] << ", "
+                << yaml_data.waypoints[pos_index][1] << ", "
+                << yaml_data.waypoints[pos_index][2] << "]" << std::endl;
     }
   }
+  
+  std::cout << "\n";  // New line after progress bar
   logger.close();
 
-  // Print time measurements and its average
+  // Print time statistics
   double mpc_avg_time = std::accumulate(mpc_times.begin(), mpc_times.end(), 0.0) / mpc_times.size();
   double sim_avg_time = std::accumulate(sim_times.begin(), sim_times.end(), 0.0) / sim_times.size();
-  double total_avg_time =
-      std::accumulate(total_times.begin(), total_times.end(), 0.0) / total_times.size();
-  std::cout << "MPC average time: " << mpc_avg_time << " s" << std::endl;
-  std::cout << "Simulator average time: " << sim_avg_time << " s" << std::endl;
-  std::cout << "Total average time: " << total_avg_time << " s" << std::endl;
+  double total_avg_time = std::accumulate(total_times.begin(), total_times.end(), 0.0) / total_times.size();
+  
+  std::cout << "\n=== Time Statistics ===" << std::endl;
+  std::cout << "MPC average time: " << mpc_avg_time * 1000 << " ms" << std::endl;
+  std::cout << "Simulator average time: " << sim_avg_time * 1000 << " ms" << std::endl;
+  std::cout << "Total average time: " << total_avg_time * 1000 << " ms" << std::endl;
+  std::cout << "Real-time factor: " << tf / total_avg_time << std::endl;
 }
 
 }  // namespace acados_mpc_examples
@@ -212,8 +225,7 @@ void test_mpc_controller(CsvLogger& logger,
 int main(int argc, char** argv) {
   // Params
   acados_mpc::acados_mpc_examples::YamlData yaml_data;
-  acados_mpc::acados_mpc_examples::read_yaml_params(
-      "/home/rafa/mpc/mpc_position/examples/simulation_config.yaml", yaml_data);
+  acados_mpc::acados_mpc_examples::read_yaml_params(config_path, yaml_data);
 
   // Initialize MPC
   acados_mpc::MPC mpc = acados_mpc::MPC();
@@ -243,12 +255,8 @@ int main(int argc, char** argv) {
   mpc.update_slack_weights_end();
   mpc.update_gains();
 
-  // Update online params
-  for (int i = 0; i < acados_mpc::OnlineParams::size_n; i++) {
-    for (int j = 0; j < acados_mpc::OnlineParams::Np; j++) {
-      mpc.get_data()->p_params.set_data(i, j, yaml_data.mpc_data.p[j]);
-    }
-  }
+  // Set mass parameter (first parameter in p_params)
+  mpc.setSolverOnlineMassParams(yaml_data.mpc_data.p[0]);
 
   // Initialize integrator
   acados_mpc::MPCSimSolver simulator = acados_mpc::MPCSimSolver();
